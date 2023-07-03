3bcfb16eaa9c23721b10a3d3ddd2205435a0dd8f
Gavin <gavc8943@gmail.com>
2023-07-03 20:49:04 +0100

    make functions for rock paper scissor game

--- script.js
+++ script.js
@@ -2,7 +2,36 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
 const choice = options[Math.floor(Math.random() * options.length)];
-console.log(choice);
+return choice;
+}
+
+function checkWinner(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissor" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+        return "Player";
+    } 
+    else {
+        return "Computer";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+const result = checkWinner(playerSelection, computerSelection);
+if(result == "Tie"){
+    return "Its a Tie!"
+}
+else if(result == "Player"){
+    return `You Win! ${playerSelection} beats ${computerSelection}`
+}
+else{
+    return `You lose! ${computerSelection} beats ${playerSelection}`
+}
 }
 
 getComputerChoice();
